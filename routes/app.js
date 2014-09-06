3f53dc892039c57ff6547f316169074fcd6fa729
Antonio <berser.ar@gmail.com>
2014-09-06 17:22:35 -0500

    layout

--- routes/app.js
+++ routes/app.js
@@ -20,6 +20,17 @@ app.use("*", function (req, res, next){
   next();
 });
 
+app.param('idBook', function (req, res, next, idBook){
+  book.findOne({_id: req.params.idBook}, function (err, _book){
+    if (err || !_book) {
+      return res.redirect('/app');
+    }
+
+    req.book = _book;
+    next();
+  });
+});
+
 app.route("/")
 .get(function (req, res) {
   book.find({}, function (err, books){
@@ -38,6 +49,37 @@ app.route('/add')
   res.render('add-book');
 })
 .post(function (req, res){
-  console.log(req.body);
-  res.redirect("back");
+  var newBook = new book(req.body);
+  newBook.save(function (err){
+    res.redirect("/app");
+  });
+});
+
+
+app.route('/delete/:idBook')
+.get(function (req, res){
+  req.book.remove(function (err){
+    return res.redirect('/app');
+  });
+});
+
+
+app.route('/edit/:idBook')
+.get(function (req, res){
+  res.render('edit-book', {
+    book: req.book
+  });
+})
+.post(function (req, res){
+  if (req.body.name && req.body.name !== req.book.name) {
+    req.book.name =  req.body.name;
+  }
+
+  if (req.body.authorName !== req.book.authorName) {
+    req.book.authorName = req.body.authorName;
+  }
+
+  req.book.save(function (err){
+    res.redirect('/app');
+  });
 });
